865b0febb5ce84922b35b8369ec2a5d3b525ab60
Running water <alsgks1218@naver.com>
2023-09-13 13:43:48 +0900

    TextField에 label이 autofill background에 가려지지 않도록 수정하고, 폼이 제출되는 지 확인

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,17 +1,49 @@
+"use client";
+
 import Image from "next/image";
+import { useForm, type SubmitHandler } from "react-hook-form";
 
 import TextField from "@/components/TextField";
 
+interface FormValues {
+  email: string;
+  password: string;
+}
+
 export default function Page() {
+  const { register, handleSubmit } = useForm<FormValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const onValid: SubmitHandler<FormValues> = (data) => {
+    console.log("제출된 폼", data);
+  };
   return (
     <div className="h-full flex flex-col justify-center md:max-w-md mx-auto">
       <div className="flex justify-center">
         <Image width={100} height={40} src="/next.svg" alt="logo" />
       </div>
       <h1 className="font-bold text-3xl text-center mt-6 mb-10">이모저모</h1>
-      <form className="block p-8 w-full bg-white border border-gray-200 rounded-lg shadow">
-        <TextField label="이메일" required />
-        <TextField label="비밀번호" type="password" required addClass="mt-6" />
+      <form
+        onSubmit={handleSubmit(onValid)}
+        className="block p-8 w-full bg-white border border-gray-200 rounded-lg shadow"
+      >
+        <TextField
+          label="이메일"
+          type="email"
+          required
+          register={register("email")}
+        />
+        <TextField
+          label="비밀번호"
+          type="password"
+          required
+          addClass="mt-6"
+          register={register("password")}
+        />
+        <button>제출</button>
       </form>
     </div>
   );
--- components/TextField.tsx
+++ components/TextField.tsx
@@ -1,8 +1,11 @@
+import type { UseFormRegisterReturn, FieldValues } from "react-hook-form";
+
 interface TextFieldProps {
   required?: boolean;
   label: string;
   addClass?: string;
   type?: "text" | "password" | "email";
+  register: UseFormRegisterReturn;
 }
 
 export default function TextField({
@@ -10,6 +13,7 @@ export default function TextField({
   label,
   addClass = "",
   type = "text",
+  register,
 }: TextFieldProps) {
   const displayLabel = required ? `${label} *` : label;
   return (
@@ -20,10 +24,11 @@ export default function TextField({
         className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-emerald-600 peer"
         placeholder={" "}
         required={required}
+        {...register}
       />
       <label
         htmlFor={label}
-        className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
       >
         {displayLabel}
       </label>
