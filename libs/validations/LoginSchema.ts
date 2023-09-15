e579e3946b902c25489094542be969318d06ee57
minhanPark <alsgks1218@naver.com>
2023-09-15 16:38:56 +0900

    LoginSchema에서 비밀번호 조건이 영문,숫자 포함 8자 이상 되도록 추가

--- libs/validations/LoginSchema.ts
+++ libs/validations/LoginSchema.ts
@@ -8,10 +8,17 @@ const LoginSchema = Joi.object({
       "string.email": "이메일 형식이 올바르지 않습니다.",
       "string.empty": "이메일을 입력해주세요.",
     }),
-  password: Joi.string().min(8).required().messages({
-    "string.min": "비밀번호가 올바르지 않습니다.",
-    "string.empty": "비밀번호를 입력해주세요.",
-  }),
+  password: Joi.string()
+    .trim()
+    .min(8)
+    .pattern(/[a-zA-Z]/, "string")
+    .pattern(/[0-9]/, "number")
+    .required()
+    .messages({
+      "string.min": "비밀번호가 올바르지 않습니다.",
+      "string.pattern.name": "비밀번호가 올바르지 않습니다.",
+      "string.empty": "비밀번호를 입력해주세요.",
+    }),
 });
 
 export default LoginSchema;
