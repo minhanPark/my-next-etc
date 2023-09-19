dd5879913932c1c6a6c771226cbdb17a5c0f6c54
Running water <alsgks1218@naver.com>
2023-09-19 19:10:54 +0900

    회원가입하면 jwt 토큰 반환하도록 하고, 쿠키에 심어줌

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,16 +1,26 @@
 import { NextResponse } from "next/server";
 import { ValidationError } from "joi";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 import SignupSchema from "@/libs/validations/SignupSchema";
 import prisma from "@/libs/prisma";
 
+interface Body {
+  email: string;
+  password: string;
+  passwordCheck: string;
+}
+
 export async function POST(request: Request) {
   try {
-    const body = await request.json();
+    const body: Body = await request.json();
     await SignupSchema.validateAsync(body);
+
+    const { email, password } = body;
+
     const existingUser = await prisma.user.findUnique({
-      where: { email: body.email },
+      where: { email },
     });
     if (existingUser) {
       return NextResponse.json({
@@ -18,18 +28,31 @@ export async function POST(request: Request) {
         message: "이미 존재하는 이메일입니다.",
       });
     }
-    const hashedPassword = await bcrypt.hash(body.password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    await prisma.user.create({
+    const user = await prisma.user.create({
       data: {
-        email: body.email,
+        email,
         hashedPassword,
       },
     });
-    return NextResponse.json({
-      ok: true,
-      message: "회원가입이 완료되었습니다.",
-    });
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string);
+
+    const response = NextResponse.json(
+      {
+        ok: true,
+        message: "회원가입이 완료되었습니다.",
+      },
+      {
+        headers: {
+          "Set-Cookie": `x-token=${token}; Path=/; HttpOnly; SameSite=Lax; Max-Age=${
+            60 * 60 * 24 * 7
+          };`,
+        },
+      }
+    );
+    return response;
   } catch (e) {
     if (e instanceof ValidationError) {
       return NextResponse.json({ ok: false, message: e.message });
