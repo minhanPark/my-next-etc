80b0d199771fc76c0256ea5d43b5ef93cbb22ab1
minhanPark <alsgks1218@naver.com>
2023-09-26 14:30:39 +0900

    user agent에 facebook 넣으니 네이버에서 메타태그 갖고오는거 확인

--- app/api/preview/route.ts
+++ app/api/preview/route.ts
@@ -1,10 +1,21 @@
 import { NextResponse } from "next/server";
 import * as cheerio from "cheerio";
 import { unfurl } from "unfurl.js";
+import ogs from "open-graph-scraper";
+
+/**
+ * rest client로 확인해보니
+ * Accept: text/html, application/xhtml+xml
+ * User-Agent: facebookexternalhit
+ * 을 붙이면 네이버도 공유할 수 있게 메타태그가 존재한다.
+ * 해당 부분을 사용해서 미리보기 형태로 만들 수 있을 것 같다.
+ */
 
 export async function POST(request: Request) {
   const body = await request.json();
   const { url } = body;
+  // const { error, html, result, response } = await ogs({ url });
+  // console.log({ error, result, response });
   const result = await unfurl(url);
   console.log(result);
 
